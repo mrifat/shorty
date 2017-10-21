9711fbeadc60979bd6dd48dab4b94627c95a80ad
mrifat <mrifat.hammad@gmail.com>
2017-10-22 05:53:23 +0900

    Added missing documentation

--- app/serializers/short_url_serializer.rb
+++ app/serializers/short_url_serializer.rb
@@ -4,19 +4,27 @@
 class ShortUrlSerializer < ActiveModel::Serializer
   attributes :startDate, :redirectCount, :lastSeenDate
 
+  # @return [String]
   def startDate
     iso_date(object.start_date)
   end
 
+  # @return [Integer]
   def redirectCount
     object.redirect_count
   end
 
+  # @return [String]
   def lastSeenDate
     return if object.last_seen_date.blank?
     iso_date(object.last_seen_date)
   end
 
+  # Converts DateTime instances to
+  #   {https://en.wikipedia.org/wiki/ISO_8601 ISO8601}
+  #   formatted date strings
+  # @param date [DateTime] Date to format
+  # @return [String]
   def iso_date(date)
     date.iso8601
   end
